3658147aca020650e0b4dbd648c1c2adfdef555d
Myeishamadkins <mmadkins@basecamecodingacademy.org>
2019-04-30 13:11:23 -0500

    edited code to run greater test cases

--- 12_Tue/day12.py
+++ 12_Tue/day12.py
@@ -1,8 +1,14 @@
 def infinite_repetition(letter, length):
     count_letter = letter.count("a")
-    string_length = max(1, len(letter[:length]))
-    result = count_letter // string_length
+    if len(letter) > 0 and letter.count("a") > 0:
+        remainder = length % len(letter)
+        string_length = letter[:remainder].count("a")
+        string = len(letter)   
+        result = (length // string) * (count_letter) + (string_length)
+    else:
+        result = 0
     return(result)
+
 
 
 if __name__ == "__main__":
@@ -14,3 +20,5 @@ if __name__ == "__main__":
     print(infinite_repetition('', 1), 'should be', 0)
     print(infinite_repetition('a', 0), 'should be', 0)
     print(infinite_repetition('ab', 1), 'should be', 1)
+    print(infinite_repetition('ab', 5), 'should be', 3)
+    
